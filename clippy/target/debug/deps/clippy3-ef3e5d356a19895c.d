080ef069fb36105bc63e151e32a1e1d7f6d963f2
Evan Williams <emw236@cornell.edu>
2023-01-02 22:56:18 -0500

    initial commit

---
+++ clippy/target/debug/deps/clippy3-ef3e5d356a19895c.d
@@ -0,0 +1,8 @@
+/Users/evanwilliams/rustlings/exercises/clippy/target/debug/deps/clippy3-ef3e5d356a19895c.rmeta: clippy3.rs Cargo.toml
+
+/Users/evanwilliams/rustlings/exercises/clippy/target/debug/deps/clippy3-ef3e5d356a19895c.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
